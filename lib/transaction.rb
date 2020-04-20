d59e015d25cccf842b6ae81971bfffcd03c95c4e
sgneha <sgneha05@gmail.com>
2020-04-20 21:21:17 +0100

    saves details of credit transaction #make_deposit

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -5,4 +5,11 @@ class Transaction
   def initialize
     @transaction_detail = {}
   end
+
+  def make_deposit(account_no, amount, balance)
+    @transaction_detail[:account_no] = account_no
+    @transaction_detail[:date] = Time.now
+    @transaction_detail[:credit] = amount
+    @transaction_detail[:balance] = balance
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -6,5 +6,11 @@ describe 'Transaction' do
   it 'initiates the transaction with a empty transaction details' do
     expect(transaction.transaction_detail).to(be {})
   end
+
+  it 'saves details of credit transaction made' do
+    transaction.make_deposit(10, 1000, 5000)
+    expect(transaction.transaction_detail[:balance]).to eq 5000
+  end
+
   
 end
