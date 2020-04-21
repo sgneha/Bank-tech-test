8d2308de51eab56faded086f66ec0636b1bd0b36
sgneha <sgneha05@gmail.com>
2020-04-21 19:01:06 +0100

    user can deposit and withdraw by bank class,#deposit_money & #withdraw_money added

--- lib/bank.rb
+++ lib/bank.rb
@@ -3,4 +3,12 @@ class Bank
   def initialize(account_no)
     @account = Account.new(account_no)
   end
+
+  def deposit_money(amount)
+    @account.deposit(amount)
+  end
+  
+  def withdraw_money(amount)
+    @account.withdraw(amount)
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,9 +1,22 @@
 require 'bank'
 describe Bank do 
+  let(:bank) { Bank.new(111) }
   describe '#initialize' do
     it 'initializes bank with a account' do
-      bank = Bank.new(10)
       expect(bank.account).to be_instance_of(Account)
     end 
+  end
+  describe '#deposit_money' do
+    it 'balance of account increases' do
+      bank.deposit_money(100)
+      expect(bank.account.balance).to eq 100
+    end
+  end
+  describe '#withdraw_money' do
+    it 'balance of account decreases' do
+      bank.deposit_money(100)
+      bank.withdraw_money(20)
+      expect(bank.account.balance).to eq 80
+    end
   end
 end
