4caab352ba28b86197a570de7012f7e10ff149d2
sgneha <sgneha05@gmail.com>
2020-04-21 16:55:03 +0100

    Instantiating user account creates an empty array of transactions

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,14 @@
 class Account
-  attr_reader :account_no, :balance
+  attr_reader :account_no, :balance, :transactions
   def initialize(account_no)
     @account_no = account_no
     @balance = 0
+    @transactions = []
  
   end  
 
   def deposit(amount)
     @balance += amount
-    
   end
 
   def withdraw(amount)
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,18 +2,26 @@ require 'account'
 
 describe Account do
   let(:account) { Account.new(20) }
-  it 'initializes with account number and balance' do
-    expect(account.balance).to eq 0
+  describe '#initialize' do
+    it 'initializes with zero balance' do
+      expect(account.balance).to eq 0
+    end
+    it 'initializes with empty transactions array' do
+      expect(account.transactions).to(be {})
+    end
   end
+  describe '#deposit' do
 
-  it 'Depositing amount increases balance' do
-    account.deposit(1000)
-    expect(account.balance).to eq 1000
+    it 'Depositing amount increases balance' do
+      account.deposit(1000)
+      expect(account.balance).to eq 1000
+    end
   end
-
-  it 'Withdrawing amount decreases balance' do
-    account.deposit(1000)
-    account.withdraw(500)
-    expect(account.balance).to eq 500
+  describe '#withdraw' do
+    it 'Withdrawing amount decreases balance' do
+      account.deposit(1000)
+      account.withdraw(500)
+      expect(account.balance).to eq 500
+    end
   end
 end
