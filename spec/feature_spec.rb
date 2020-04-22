d2cc214d104fdb41d19d458c26480e335331a994
sgneha <sgneha05@gmail.com>
2020-04-22 22:28:04 +0100

    Added feature test user can print statement,and also blank statement when no transactions

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -12,4 +12,13 @@ describe 'User experience in bank' do
     account.deposit(1000)
     expect { account.withdraw(2000) }.to raise_error 'Not sufficient balance'
   end
+  it 'can print bank statement' do
+    @time = Time.now.strftime("%d/%m/%Y")
+    account.deposit(1000)
+    account.withdraw(500)
+    expect { account.print_statement }.to output("date || credit || debit || balance \n#{@time}|| || 500.00 ||500.00\n#{@time}||  1000.00 || ||1000.00").to_stdout
+  end
+  it 'can print blank bank statement if no transactions' do
+    expect { account.print_statement }.to output("date || credit || debit || balance ").to_stdout
+  end
 end
