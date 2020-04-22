360f50bd82745ef68d9061969051bac28a126906
sgneha <sgneha05@gmail.com>
2020-04-22 22:09:32 +0100

    Added feature test of withdrawal and getting successful msg and error

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,9 +1,15 @@
 require 'account'
-describe 'User experience in bank features' do
+describe 'User experience in bank' do
   let(:account) { Account.new }
   it 'can deposit money' do
-   
     expect(account.deposit(1000)).to eq "Credit of amount £1000 was successfull"
-      
+  end
+  it 'can withdraw money' do
+    account.deposit(1000)
+    expect(account.withdraw(500)).to eq "Debit of amount £500 was successfull"
+  end
+  it 'gets error when withdrawal amount is greater than balance' do
+    account.deposit(1000)
+    expect { account.withdraw(2000) }.to raise_error 'Not sufficient balance'
   end
 end
