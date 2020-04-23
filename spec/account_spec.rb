8294ea73748595eb7756c32fc13f1c6a16376f01
sgneha <sgneha05@gmail.com>
2020-04-23 13:05:13 +0100

    Refactoring some tests and print statement test for account class

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,45 +11,54 @@ describe Account do
     end
   end
   describe '#deposit' do
+    before do
+      account.deposit(1000)
+    end
 
     it 'Depositing amount increases balance' do
-      account.deposit(1000)
       expect(account.balance).to eq 1000
     end
 
     it 'Deposit Transaction is created' do
-      account.deposit(1000)
       expect(account.transactions.first).to be_instance_of(Transaction)
     end
-    
 
+    it 'Second deposit Transaction created has credit amount ' do
+      account.deposit(2000)
+      expect(account.transactions[1].amount).to eq 2000
+    end
   end
   describe '#withdraw' do
-    it 'Withdrawing amount decreases balance' do
+    before do
       account.deposit(1000)
+    end
+    it 'Withdrawing amount decreases balance' do
       account.withdraw(500)
       expect(account.balance).to eq 500
     end
 
     it 'Withdrawal transaction is created' do
-      account.deposit(1000)
       account.withdraw(500)
       expect(account.transactions[1]).to be_instance_of(Transaction)
     end
+
+    it 'Second withdrawal transaction created has debit amount ' do
+      account.withdraw(500)
+      account.withdraw(300)
+      expect(account.transactions[2].amount).to eq 300
+    end
+
     it 'Withdrawal not successful due to less balance' do
-      account.deposit(1000)
       expect { account.withdraw(1500) }.to raise_error 'Not sufficient balance'
     end
 
   end
-
-  # describe '#print_statement' do
-  #   it 'prints statement' do
-  #     account.deposit(1000)
-  #     account.withdraw(500)
-  #     expect(account.print_statement).to eq account.transactions
-  #   end
-
-    
-  # end
+  describe '#print_statement' do
+    it 'Prints details of all transactions' do
+      account.deposit(1000)
+      @time = Time.now
+      allow(Time).to receive(:now).and_return(@time)
+      expect { account.print_statement }.to output("date || credit || debit || balance \n#{@time.strftime("%d/%m/%Y")}||  1000.00 || ||1000.00").to_stdout
+    end
+  end
 end
